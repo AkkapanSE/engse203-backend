0202c609d35f637963fa0e64d4b7b5464d62550b
AkkapanSE <akkapan_to67@live.rmutl.ac.th>
2025-09-10 13:54:33 +0700

    test

---
+++ nodejs-database-lab/db.sql
@@ -0,0 +1,5 @@
+INSERT INTO Students (firstName, lastName, email, age, major)
+VALUES ('Somshy', 'Jaidee', 'somchai@email.com', 20, 'Computer Science');
+
+INSERT INTO Students (firstName, lastName, email, age, major)
+VALUES ('Aomying', 'Luckdee', 'somying@email.com', 19, 'Software Engineering');
